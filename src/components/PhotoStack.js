4167b329ffdd33981c89b61f8ebb2784b7e8befb
Nabin Purja <>
2021-02-24 11:54:04 +0000

    Basic animate element out on drag end if beyond

--- src/components/PhotoStack.js
+++ src/components/PhotoStack.js
@@ -13,30 +13,75 @@ const data = [
 ];
 
 const PhotoStack = () => {
+    const [stack, setStack] = useState(data);
+    const [current, setCurrent] = useState(data[data.length - 1].id);
     const controls = useAnimation();
+
+    useEffect(() => {
+        console.info('useEffect.stack', stack);
+        setCurrent(stack[stack.length - 1].id);
+    }, [stack]);
+
+    useEffect(() => {
+        console.info('useEffect.stack', current);
+    }, [current]);
+
+    const handleDragEnd = (info) => {
+        const dragOffSetX = info.offset.x;
+        if (dragOffSetX > -450 && dragOffSetX < 450) return;
+
+        const direction = dragOffSetX < 0 ? -1 : 1;
+        const endX = (window.screen.width / 2) * direction;
+        const rotation = 5 * direction;
+
+        controls.start({
+            x: endX,
+            y: info.offset.y,
+            rotate: rotation,
+            transition: { duration: 0.5 },
+        });
+    };
+
+    const onComplete = () => {
+        console.info('onAnimationEnd');
+        setStack(stack.filter((photo) => photo.id !== stack.length - 1));
+    };
 
     return (
         <div className='photo-stack flex justify-center my-20'>
             <div className='photos'>
-                {data.map((photo) => (
-                    <motion.div key={photo.id}>
+                {stack.map((photo) => {
+                    if (photo.id === current) {
+                        return (
+                            <motion.img
+                                drag
+                                dragConstraints={{ left: 0, right: 0, top: 0, bottom: 0 }}
+                                dragElastic={0.7}
+                                onDrag={(event, info) => {
+                                    console.log(info.point.x);
+                                }}
+                                onDragEnd={(event, info) => {
+                                    handleDragEnd(info);
+                                }}
+                                animate={controls}
+                                onAnimationComplete={onComplete}
+                                key={photo.id}
+                                className='photo'
+                                src={photo.source}
+                                alt=''
+                            />
+                        );
+                    }
+                    return (
                         <motion.img
-                            drag
-                            dragConstraints={{ left: 0, right: 0, top: 0, bottom: 0 }}
-                            dragElastic={0.7}
-                            onDrag={(event, info) => {
-                                // console.log('onDragEnd', info.offset.x);
-                            }}
-                            onDragEnd={(event, info) => {
-                                console.log('onDragEnd', info.offset.x);
-                            }}
+                            key={photo.id}
                             className='photo'
                             src={photo.source}
-                            animate={controls}
                             alt=''
+                            draggable={false}
                         />
-                    </motion.div>
-                ))}
+                    );
+                })}
             </div>
         </div>
     );
